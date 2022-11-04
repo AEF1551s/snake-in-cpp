38749797d8874f760da8477740b64fdd7417df94
Krisjanis <ivbulisk@gmail.com>
2022-11-04 17:04:56 +0200

    updated .gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/KRISH/Documents/snake_cpp
-# Build directory: C:/Users/KRISH/Documents/snake_cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
