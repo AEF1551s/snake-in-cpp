5ac9b0c1066da5d99fa455200b197d0d7942e199
Krisjanis <ivbulisk@gmail.com>
2022-11-07 23:05:50 +0200

    updating .gitignore

--- main.cpp
+++ main.cpp
@@ -3,15 +3,13 @@
 #include <cstring>
 #include <chrono>
 #include <thread>
-// using std::cout;
+#include <cstdlib>
+
 #define size_x 20
 #define size_y 20
 
-void draw(char *display[20][20])
+void draw(char *display[20][20], int time_scale)
 {
-    // time in miliseconds
-    int time_scale = 250;
-
     std::string line0 = "";
 
     // convert array to single string
@@ -27,54 +25,78 @@ void draw(char *display[20][20])
     // output display array in string line0
     std::cout << line0 << std::endl;
 
-    // delay 50ms
     std::cout.flush();
+    // time scale
     std::this_thread::sleep_for(std::chrono::milliseconds(time_scale));
     system("CLS");
 }
 
-/*
-TODO add fruit spawn logic
-TODO add keyboard inputs
-TODO add snake movement logic, passtrough borders
-TODO snake lenght changes, eating
-*/
+void spawn_fruit(char *display[20][20], bool eaten)
+{
+    int randx = (rand() % (size_x - 1) + 1);
+    int randy = (rand() % (size_y - 1) + 1);
+    *display[randy][randx] = 'o';
+    // add so fruit doesnt spawn on snake, that means that is cant also spawn in the same location again.
+    // if snake has eaten fruit, spawn another; previous fruit was deleted by snake head.
+}
+
+void input()
+{
+    // input gets user keys, then outputs nice enums for snake function to use.
+}
+
+void snake()
+{
+    /*
+    snake gets directions, then moves. if snake has eaten a fruit then snake lenght++.
+    if snake crashes into itself then game over, return some value
+    move snake trough borders
+    */ 
+}
+
+void display_fill(char *display[20][20])
+{
+
+    // draw display borders
+    for (int i = 0; i < size_y; i++)
+    {
+        *display[0][i] = 'x';
+        *display[i][0] = 'x';
+        *display[size_y - 1][i] = 'x';
+        *display[i][size_y - 1] = 'x';
+    }
+
+    // display fill
+    for (int i = 1; i < size_y - 1; i++)
+    {
+        for (int j = 1; j < size_x - 1; j++)
+        {
+            *display[i][j] = '.';
+        }
+    }
+}
+
 int main(void)
 {
     char display[size_y][size_x];
     char *display_ptr[size_y][size_x];
-    int display_size = sizeof(display) / sizeof(display[0]);
 
     // initialize display_ptr
-    for (int i = 0; i < display_size; i++)
+    for (int i = 0; i < size_y; i++)
     {
-        for (int j = 0; j < display_size; j++)
+        for (int j = 0; j < size_x; j++)
         {
             display_ptr[i][j] = &display[i][j];
         }
     }
 
-    // draw display borders
-    for (int i = 0; i < size_y; i++)
-    {
-        *display_ptr[0][i] = 'x';
-        *display_ptr[i][0] = 'x';
-        *display_ptr[size_y - 1][i] = 'x';
-        *display_ptr[i][size_x - 1] = 'x';
-    }
-
-    // display fill
-    for (int i = 1; i < display_size - 1; i++)
-    {
-        for (int j = 1; j < display_size - 1; j++)
-        {
-            *display_ptr[i][j] = '.';
-        }
-    }
     int i = 0;
+    // initialize display with 'x' borders and '.' screen
+    display_fill(display_ptr);
+
     while (1)
     {
         std::cout << "frame: " << i++ << std::endl;
-        draw(display_ptr);
+        draw(display_ptr, 20);
     }
 }
